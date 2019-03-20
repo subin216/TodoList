122a0126e5e2742b44f2913f1e40c4b199f91201
subinb <subin_bae@sfu.ca>
2019-03-20 14:45:34 -0700

    implemented basic features

---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import Header from './components/layout/Header'
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import './App.css';
+
+class App extends Component {
+	state = {
+		todos: [
+			{
+				id: 1,
+				title: 'Take out the trash',
+				completed:false
+			},
+			{
+				id: 2,
+				title: 'Dinner with wife',
+				completed:false
+			},
+			{
+				id: 3,
+				title: 'Meeting with boss',
+				completed:false
+			}
+		]
+	}
+
+	// Toggle Complete
+	markComplete = (id) => {
+		this.setState({ todos: this.state.todos.map(todo => {
+			if(todo.id === id) {
+				todo.completed = !todo.completed
+			}
+			return todo;
+		}) });
+		console.log(id)
+	}
+
+	//Delete Todo
+	delTodo = (id) => {
+		this.setState({todos: [...this.state.todos.filter(todo => todo.id
+			!== id)] });
+	}
+
+	render() {
+	    return (
+	      <div className="App">
+	      	<div className="container">
+		      	<Header />
+		      	<AddTodo />
+		        <Todos todos={this.state.todos} markComplete={this.markComplete}
+		        delTodo={this.delTodo} />
+	        </div>
+	      </div>
+	    );
+  	}
+}
+
+export default App;
