97f04f1f295f9d793289337ec9413e51ba479350
subinb <subin_bae@sfu.ca>
2019-03-24 08:52:45 -0700

    Updates

---
+++ precache-manifest.28cc7c54379f00fb5dcdbecfd31318e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23ef1ee30c4a1e2cab36",
+    "url": "/TodoList/static/js/runtime~main.90b1048d.js"
+  },
+  {
+    "revision": "33dc27ff758588f3910b",
+    "url": "/TodoList/static/js/main.c402b7f3.chunk.js"
+  },
+  {
+    "revision": "37633691b9724b784dbc",
+    "url": "/TodoList/static/js/2.ff439d2d.chunk.js"
+  },
+  {
+    "revision": "33dc27ff758588f3910b",
+    "url": "/TodoList/static/css/main.7298bd65.chunk.css"
+  },
+  {
+    "revision": "056e692027082572dd2f85990be4ee98",
+    "url": "/TodoList/index.html"
+  }
+];
